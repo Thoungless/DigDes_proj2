c0cb27a753a1c370d9d473d70ae6a7d99e747dc8
Ivan <fffff2033@gmail.com>
2023-05-26 14:34:05 +0300

    aa
    aa

---
+++ Задание2/WarAndPease/WordCounter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WordCounter
+{
+    public class WordCounter
+    {
+        private Dictionary<string, int> CountWords(string text)
+        {
+            string l = "<dd>&nbsp;&nbsp;";
+            text = text.Substring(text.IndexOf(l) + l.Length);
+            text = Regex.Replace(text, "<.*?>", String.Empty);
+            text = Regex.Replace(text, @"\r", String.Empty);
+            text = Regex.Replace(text, @"\n", String.Empty);
+            text = Regex.Replace(text, @"&nbsp;", String.Empty);
+
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            char[] delimiters = { ' ', '.', ',', ';', '!', '?' };
+
+            string[] words = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                string lowercaseWord = word.ToLower();
+
+                if (wordCounts.ContainsKey(lowercaseWord))
+                {
+                    wordCounts[lowercaseWord]++;
+                }
+                else
+                {
+                    wordCounts[lowercaseWord] = 1;
+                }
+            }
+
+            var wordCountsDesc = wordCounts.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            return wordCountsDesc;
+       
+        }
+
+        public Dictionary<string, int> CountWordsMultiThreaded(string text)
+        {
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+            Parallel.Invoke(() =>
+            {
+                wordCounts = CountWords(text);
+            });
+
+            return wordCounts;
+        }
+    }
+}
---
+++ Задание2/Задание2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DigDes_2_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = (@"http://az.lib.ru/t/tolstoj_lew_nikolaewich/text_0040.shtml");
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\book.txt";
+            Dictionary<string, int> res = new Dictionary<string, int>();
+
+            WebRequest req = WebRequest.Create(book);
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            StreamReader sr = new StreamReader(stream, Encoding.GetEncoding("windows-1251"));
+            string text = sr.ReadToEnd();
+
+
+            Stopwatch privateMethodStopwatch = new Stopwatch();
+            Stopwatch publicMethodStopwatch = new Stopwatch();
+
+            Type type = typeof(WordCounter.WordCounter);
+            Type type2 = typeof(WordCounter.WordCounter);
+
+            MethodInfo methodInfo = type.GetMethod("CountWords", BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo methodInfo2 = type.GetMethod("CountWordsMultiThreaded", BindingFlags.Public | BindingFlags.Instance);
+
+            if (methodInfo != null && methodInfo2 != null)
+            {
+                WordCounter.WordCounter wc = new WordCounter.WordCounter();
+                WordCounter.WordCounter wc2 = new WordCounter.WordCounter();
+                object[] parametrs = { text };
+
+                privateMethodStopwatch.Start();
+                res = methodInfo.Invoke(wc, parametrs) as Dictionary<string, int>;
+                privateMethodStopwatch.Stop();
+
+                res = null;
+
+                publicMethodStopwatch.Start();
+                res = methodInfo2.Invoke(wc2, parametrs) as Dictionary<string, int>;
+                publicMethodStopwatch.Stop();
+
+                Console.WriteLine($"Приватный метод: Обработка текста завершена.");
+                Console.WriteLine($"Приветный метод обработкой: Время выполнения: {privateMethodStopwatch.ElapsedMilliseconds} мс");
+
+
+                Console.WriteLine($"Публичный метод с многопоточной обработкой: Обработка текста завершена.");
+                Console.WriteLine($"Публичный метод с многопоточной обработкой: Время выполнения: {publicMethodStopwatch.ElapsedMilliseconds} мс");
+            }
+            else
+            {
+                Console.WriteLine("Метод не найден");
+            }
+
+            foreach (var item in res)
+            {
+                using (StreamWriter sw = new StreamWriter(path, true))
+                {
+                    sw.WriteLine(item);
+                }
+            }
+        }
+    }
+}
